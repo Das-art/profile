3d9c9e76b2daba1d5912ade3533348ae53ae391a
NIhal Das <nihaldasp@gmail.com>
2023-09-18 23:32:23 +0530

    first commit2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const nodemailer = require('nodemailer');
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 const path= require('path')
 app.use(cors());
 app.use(express.json());
